4d1b43c00cb7476c0f2138f46fef11ae9a2ce0f7
weblw <872993425@qq.com>
2020-06-21 12:55:49 +0800

    继续完成作业

--- work/index.js
+++ work/index.js
@@ -108,6 +108,9 @@ class Maybe {
   constructor (x) {
     this._value = x
   }
+  constructor (x) {
+    this._value = x
+  }
   map (fn) {
     return this.isNothing() ? this : Maybe.of(fn(this._value))
   }
@@ -123,6 +126,197 @@ const fp = require('lodash/fp')
 
 let maybe = Maybe.of([5, 6, 1])
 let ex1 = () => {
-  maybe.map(fp.add, fp.map)
+  maybe.map(fp.map(fp.add))
 }
 console.log(ex1())
+
+// 练习2
+const fp = require('lodash/fp')
+const { MayBe, Container } = require('./support')
+
+let xs = Container.of(['do', 'ray', 'so', 'la'])
+let ex2 = () => {
+  xs.map(fp.first)
+}
+
+// 练习3
+const fp = require('lodash/fp')
+const { MayBe, Container } = require('./support')
+
+let safeProp = fp.curry(function (x, o) {
+  return Maybe.of(o[x])
+})
+let user = { is: 2, name: 'Albert' }
+let ex3 = () => {
+  safeProp('name').map(fp.first)
+}
+
+// 练习4
+const fp = require('lodash/fp')
+const { MayBe, Container } = require('./support')
+
+let ex4 = function (n) {
+  // if (n) {
+  //   return parseInt(n)
+  // }
+  MayBe.of(parseInt(n))
+}
+
+const PENDING = 'pending' // 等待
+const FULLFILLED = 'fullfilled' // 成功
+const REJECTED = 'rejected' // 失败
+
+class MyPromise {
+  constructor (executor) {
+    try {
+      executor(this.resolve, this.reject)
+    } catch (e) {
+      this.reject(e)
+    }
+  }
+  // promise 状态
+  status = PENDING
+  // 成功之后的值
+  value = undefined
+  // 失败之后的原因
+  reason = undefined
+  // 成功回调
+  successCallback = []
+  // 失败回调
+  failCallback = []
+
+  resolve = value => {
+    // 如果状态不是等待，组织程序向下执行
+    if (this.status !== PENDING) return
+    // 将状态更改为成功
+    this.status = FULLFILLED
+    // 保存成功之后的值
+    this.value = value
+    // 判断成功回调是否存在 如果存在 调用
+    while (this.successCallback.length) this.successCallback.shift()()
+  }
+
+  reject = reason => {
+    // 如果状态不是等待，组织程序向下执行
+    if (this.status !== PENDING) return
+    // 将状态更改为失败
+    this.status = REJECTED
+    // 保存失败后的原因
+    this.reason = reason
+    // 判断失败回调是否存在 存在 调用
+    while (this.failCallback.length) this.failCallback.shift()()
+  }
+
+  then (successCallback, failCallback) {
+    // 参数可选
+    successCallback = successCallback ? successCallback : value => value
+    failCallback = failCallback
+      ? failCallback
+      : reason => {
+          throw reason
+        }
+    let promise = new MyPromise((resolve, reject) => {
+      // 判断状态
+      if (this.status === FULLFILLED) {
+        setTimeout(() => {
+          try {
+            let x = successCallback(this.value)
+            resolvePromise(promise, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0)
+      } else if (this.status === REJECTED) {
+        setTimeout(() => {
+          try {
+            let x = failCallback(this.reason)
+            resolvePromise(promise, x, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        }, 0)
+      } else {
+        // 将回调函数存起来
+        this.successCallback.push(() => {
+          setTimeout(() => {
+            try {
+              let x = successCallback(this.value)
+              resolvePromise(promise, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0)
+        })
+        this.failCallback.push(() => {
+          setTimeout(() => {
+            try {
+              let x = successCallback(this.reason)
+              resolvePromise(promise, x, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          }, 0)
+        })
+      }
+    })
+    return promise
+  }
+  finally (callback) {
+    return this.then(
+      value => {
+        return MyPromise.resolve(callback()).then(() => value)
+      },
+      reason => {
+        return MyPromise.resolve(callback()).thn(() => {
+          throw reason
+        })
+      }
+    )
+  }
+  catch (failCallback) {
+    return this.then(undefined, failCallback)
+  }
+  static all (array) {
+    let result = []
+    let index = 0
+    return new MyPromise((resolve, reject) => {
+      function addData (key, value) {
+        result[key] = value
+        index++
+        if (index === array.length) {
+          resolve(result)
+        }
+      }
+      for (let i = 0; i < array.length; i++) {
+        let currrent = array[i]
+        if (currrent instanceof MyPromise) {
+          currrent.then(
+            value => addData(i, value),
+            reason => reject(reason)
+          )
+        } else {
+          addData(i, array[i])
+        }
+      }
+    })
+  }
+  static resolve (value) {
+    if (value instanceof MyPromise) value
+    return new MyPromise(resolve => resolve(value))
+  }
+}
+
+function resolvePromise (promise, x, resolve, reject) {
+  if (promise === x) {
+    return reject(
+      new TypeError('Chaining cycle detected for promise #<Promise>')
+    )
+  }
+  if (x instanceof MyPromise) {
+    x.then(resolve, reject)
+  } else {
+    resolve(x)
+  }
+}
+
+module.exports = MyPromise
